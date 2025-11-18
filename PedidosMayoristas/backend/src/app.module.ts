407991e6bb75d167265cfa33f3ccf2428c73fc3b
AgusNoriega <agusnori88@gmail.com>
2025-11-18 09:17:23 -0300

    Limpiamos la Rama Insane

--- PedidosMayoristas/backend/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from 'prisma/prisma.module';
-import { PedidosModule } from './pedidos/pedidos.module';
-
-@Module({
-  imports: [PrismaModule, PedidosModule],
-})
-export class AppModule {}
